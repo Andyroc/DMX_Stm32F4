908c7dc8eb155dd77d90e81781e9b086d395c45c
Andy Roc <64015489+Andyroc@users.noreply.github.com>
2021-01-15 05:11:38 +0300

    Update DMX_Stm32f4_lib.h

--- DMX_Stm32f4_lib.h
+++ DMX_Stm32f4_lib.h
@@ -24,7 +24,7 @@ byte OUT6 = -1;
 byte OUT7 = -1;
 byte OUT8 = -1;
 
-#define OUTPORT GPIOC // output port GPIOA, GPIOB, GPOC ....
+#define OUTPORT GPIOA // output port GPIOA, GPIOB, GPOC ....
 
 void DMX_begin()
 {
